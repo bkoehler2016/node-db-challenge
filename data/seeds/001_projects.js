3d9238d86d256b4d3dcf9537ac8285c0b8fea1aa
Ben Koehler <benkoehler@Bens-MacBook-Pro.local>
2020-04-17 18:52:10 -0700

    Fixed issue with database

--- data/seeds/001_projects.js
+++ data/seeds/001_projects.js
@@ -1,13 +1,25 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("projects")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("projects").insert([
+        {
+          name: "Writing",
+          description: "Write a page a day",
+          completed: false
+        },
+        {
+          name: "Exercising",
+          description: "Running and Pilates",
+          completed: false
+        },
+        {
+          name: "Meditating",
+          description: "Vipassana method",
+          completed: false
+        }
       ]);
     });
 };
